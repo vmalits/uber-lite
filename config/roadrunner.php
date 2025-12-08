23d7c9a376c76caf4983b7fd769bebb8ad697e81
Vladimir Malit <vladimir.malits@gmail.com>
2025-12-08 21:05:56 +0200

    Add RoadRunner configuration and update dependencies in composer.json

---
+++ config/roadrunner.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use Spiral\RoadRunner\Environment\Mode;
+use Spiral\RoadRunnerLaravel\Grpc\GrpcWorker;
+use Spiral\RoadRunnerLaravel\Http\HttpWorker;
+use Spiral\RoadRunnerLaravel\Queue\QueueWorker;
+use Spiral\RoadRunnerLaravel\Temporal\TemporalWorker;
+use Temporal\Worker\WorkerFactoryInterface as TemporalWorkerFactoryInterface;
+
+return [
+    'cache' => [
+        'storage' => 'cache',
+    ],
+
+    'grpc' => [
+        'services' => [
+            // GreeterInterface::class => new Greeter::class,
+        ],
+    ],
+
+    'temporal' => [
+        'address' => env('TEMPORAL_ADDRESS', '127.0.0.1:7233'),
+        'defaultWorker' => env('TEMPORAL_TASK_QUEUE', TemporalWorkerFactoryInterface::DEFAULT_TASK_QUEUE),
+        'workers' => [],
+        'declarations' => [
+            // 'App\Temporal\GreeterWorkflow'
+        ],
+    ],
+
+    'workers' => [
+        Mode::MODE_HTTP => HttpWorker::class,
+        Mode::MODE_JOBS => QueueWorker::class,
+        Mode::MODE_GRPC => GrpcWorker::class,
+        Mode::MODE_TEMPORAL => TemporalWorker::class,
+    ],
+];
