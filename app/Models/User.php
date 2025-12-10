b9a2b227ffc6a96a5594219916c71579896a39f8
Vladimir Malit <vladimir.malits@gmail.com>
2025-12-10 13:13:09 +0200

    Refactor migration files to use ULID for primary keys and update User model to support ULIDs

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,18 +5,21 @@ declare(strict_types=1);
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory;
+    use HasUlids;
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are mass-assignable.
      *
      * @var list<string>
      */
